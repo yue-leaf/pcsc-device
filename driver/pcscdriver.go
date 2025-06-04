6cc1cefb6793ec04152a29b010963de67bc3f604
weiqi.lei <weiqi.lei@snowballtech.com>
2025-06-04 15:15:08 +0800

    adjust: 1、调整不规范命名 2、调整日志打印 3、取消依然取消服务关闭前将设备状态更新为down的能力，edgex框架会在执行stop前注销服务，导致更新设备状态时网络连接不通。

--- driver/pcscdriver.go
+++ driver/pcscdriver.go
@@ -337,9 +337,9 @@ func (s *PcscDriver) Stop(force bool) error {
 	if s.lc != nil {
 		edgeXLog.Warnf("PcscDriver.Stop called: force=%v", force)
 	}
-	oldSerialNumberReaderMap := s.getAllSerialNumberReaderMap()
-	edgeXLog.Infof("modify all devices to down:%s", oldSerialNumberReaderMap)
-	s.deleteOldDevice(&edgeXLog, oldSerialNumberReaderMap, []string{})
+	//oldSerialNumberReaderMap := s.getAllSerialNumberReaderMap()
+	//edgeXLog.Infof("modify all devices to down:%s", oldSerialNumberReaderMap)
+	//s.deleteOldDevice(&edgeXLog, oldSerialNumberReaderMap, []string{})
 	log.Close()
 	return s.client.Release()
 }
